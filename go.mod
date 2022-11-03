13dcdacff29720ac9c21d5b9adeb72e644f6746e
Rainy <939029835@qq.com>
2022-11-03 18:38:30 +0800

    新增OSS镜像源支持

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module haxelib/v2
 
 go 1.19
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.5+incompatible // indirect
+	golang.org/x/time v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,79 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 )
 
+var (
+	OssId       = flag.String("ossid", "", "阿里云OSS_ACCESSKEY_ID")
+	OssSecret   = flag.String("osssecret", "", "阿里云OSS_ACCESSKEY_SECRET")
+	OssEndpoint = flag.String("endpoint", "", "阿里云Endpoint")
+	OssBucket   = flag.String("bucket", "none", "阿里云Bucket")
+	Port        = flag.Int("port", 80, "阿里云Bucket")
+)
+
+// pathExists 判断一个文件或文件夹是否存在
+// 输入文件路径，根据返回的bool值来判断文件或文件夹是否存在
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+// 上传到OSS服务器
+func uploadOSS(filename string, data []byte) {
+	// 移除第一个/
+	filename = strings.Join(strings.Split(filename, "")[1:], "")
+	fmt.Println("开始镜像到OSS", filename)
+	exist, _ := pathExists(filename)
+	if exist {
+		fmt.Println("已经在镜像进行中")
+		return
+	}
+	client, err := oss.New(*OssEndpoint, *OssId, *OssSecret)
+	if err != nil {
+		fmt.Println("无法链接Oss服务器", err.Error())
+		return
+	}
+	bucket, err := client.Bucket(*OssBucket)
+	if err != nil {
+		fmt.Println("无法链接Bucket:"+*OssBucket, err.Error())
+		return
+	}
+	// 判断是否已经镜像好了
+	existObject, _ := bucket.IsObjectExist(filename)
+	if existObject {
+		fmt.Println(filename, "已镜像")
+		return
+	}
+	werr := ioutil.WriteFile(filename, data, 0666)
+	if werr != nil {
+		fmt.Println("WriteFile Error:", werr.Error())
+		return
+	}
+	err = bucket.PutObjectFromFile(filename, filename)
+	if err != nil {
+		fmt.Println("文件"+filename+"无法上传到OSS", err.Error())
+	} else {
+		fmt.Println("镜像成功", filename)
+	}
+	os.Remove(filename)
+}
+
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("请求地址：", r.URL.Path, r.Method)
 		index := strings.Index(r.URL.Path, "files/3.0/")
@@ -19,6 +85,8 @@ func main() {
 				defer rep.Body.Close()
 				bytes, b := ioutil.ReadAll(rep.Body)
 				if b == nil {
+					// 开始上传到OSS
+					go uploadOSS(r.URL.Path, bytes)
 					w.Write(bytes)
 				} else {
 					fmt.Println("请求错误：", b.Error())
@@ -28,11 +96,15 @@ func main() {
 			w.Write([]byte("Not support the Path"))
 		}
 	})
-	err := http.ListenAndServe(":80", nil)
+	dir := "files/3.0/"
+	direrr := os.MkdirAll(dir, 0777)
+	if direrr != nil {
+		panic(direrr)
+	}
+	fmt.Println("服务器启动：" + fmt.Sprint(*Port))
+	err := http.ListenAndServe(":"+fmt.Sprint(*Port), nil)
 	if err != nil {
 		fmt.Println("服务器错误：", err.Error())
 		panic(err)
-	} else {
-		fmt.Println("服务器已启动")
 	}
 }
